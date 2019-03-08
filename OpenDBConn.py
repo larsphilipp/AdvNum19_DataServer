dee9c69eb98c7b83482251e56100a5d5d95115db
Lars Stauffenegger <lst@ads.cat-holding.ch>
2019-03-08 18:40:25 +0100

    test ignore

--- OpenDBConn.py
+++ OpenDBConn.py
@@ -1,3 +1,33 @@
+## Title:        EOD Prices Quandl
+## Author:       Elisa FLeissner, Lars Stauffenegger, Peter la Cour
+## Email:        elisa.fleissner@student.unisg.ch,
+##               lars.stauffenegger@student.unisg.ch,
+##               peter.lacour@student.unisg.ch
+## Place, Time:  Zürich, 08.03.19
+## Description:  Gets all the news of the specified companies in the Underlyings
+##               database from Quandl and adds it to the Price Table
+## Improvements: -
+## Last changes: -
+
+#-----------------------------------------------------------------------------#
+# Loading Packages
+import quandl
+
+## DB Connection (includes loading of relevant data)
+db = DBConn()
+
+# Quandl Authentication
+quandl.ApiConfig.api_key = db.apiKeyObject
+
+# Fetch Prices and Insert to DB
+for ticker in db.tickerObject:
+    db._insertQuandlPrices(ticker["Ticker"], quandl.get(db._getDataType("Quandl") + "/" + ticker["Ticker"], rows = 1))
+
+db.CloseConn()
+
+
+
+
 ## Title:        Database Connection
 ## Author:       Elisa FLeissner, Lars Stauffenegger, Peter la Cour
 ## Email:        elisa.fleissner@student.unisg.ch,
@@ -12,49 +42,44 @@
 #-----------------------------------------------------------------------------#
 # Loading Packages
 import pymysql
-import json
 
 ## Class as Instance of a Requests Session
 class DBConn():
-
+    
     def __init__(self):
-        
-        # Read config.json
-        with open('C:/Users/LST/Documents/Uni/Advanced Numerical Methods/AdvNum19_DataServer/config.json') as json_file:  
-            credentials = json.load(json_file)
-
         dbServerName    = "localhost"
-        self.dbUser     = credentials['dataserverDB']['user']
-        dbPassword      = credentials['dataserverDB']['password']
+        self.dbUser     = "root"
+        dbPassword      = "advnum19"
         self.dbName     = "dataserver"
         charSet         = "utf8mb4"
         cursorType      = pymysql.cursors.DictCursor
-
+        
         # Cursor
         self.connectionObject = pymysql.connect(host=dbServerName, user=self.dbUser, password=dbPassword, db=self.dbName, charset=charSet,cursorclass=cursorType)
         self.cursorObject = self.connectionObject.cursor()
-
+        
         # Load Data
         self.tickerObject = self._getTickers()
         self.apiKeyObject = self._getAPIKey()
-
+        
     def _getTickers(self):
         self.cursorObject.execute("SELECT Ticker FROM Underlyings")
         return self.cursorObject.fetchall()
-
+    
     def _getDataType(self, source):
         self.cursorObject.execute("SELECT DataType FROM RequestData WHERE Source = '{}' ".format(source))
         return self.cursorObject.fetchall()[0]["DataType"]
-
+    
     def _getAPIKey(self):
         self.cursorObject.execute("SELECT APIKey FROM Authentications WHERE User = '{}' ".format(self.dbUser))
         return self.cursorObject.fetchall()[0]["APIKey"]
-
+    
     def _insertQuandlPrices(self, ticker, quandlData):
         date = quandlData.index[0].date().strftime('%Y-%m-%d')
         self.cursorObject.execute("INSERT IGNORE INTO Prices (Date, Ticker, Open, High, Low, Close, Volume, Dividend, Split, Adj_Open, Adj_High, Adj_Low, Adj_Close, Adj_Volume) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(date, ticker, float(quandlData.Open[0]), float(quandlData.High[0]), float(quandlData.Low[0]), float(quandlData.Close[0]), float(quandlData.Volume[0]), float(quandlData.Dividend[0]), float(quandlData.Split[0]), float(quandlData.Adj_Open[0]), float(quandlData.Adj_High[0]), float(quandlData.Adj_Low[0]), float(quandlData.Adj_Close[0]), float(quandlData.Adj_Volume[0])))
         self.connectionObject.commit()
-
+    
     def CloseConn(self):
         # Close the database connection
         self.connectionObject.close()
+
--- YahooFinanceNews.py
+++ YahooFinanceNews.py
@@ -7,28 +7,28 @@
 ## Place, Time:  St. Gallen, 07.03.19
 ## Description:  Gets all the news of the specified companies in the Underlyings
 ##               database from Yahoo Finance and adds it to the TickerNews database
-## Improvements: Move time to yesterday if timestamp is negative
+## Improvements: -
 ## Last changes: -
 
 #-----------------------------------------------------------------------------#
 # Loading Packages
 #-----------------------------------------------------------------------------#
-from    DatabaseConnection  import *
-from    bs4                 import BeautifulSoup    as bs
-from    sqlalchemy          import create_engine
-from    sqlalchemy          import update
-import  sqlalchemy          as db
-import  pandas              as pd
-import  numpy               as np
+from    bs4         import BeautifulSoup    as bs
+from    sqlalchemy  import create_engine
+from    sqlalchemy  import update
+import  sqlalchemy  as db
+import  pandas      as pd
+import  numpy       as np
 import  requests
 import  pymysql
 import  datetime
+
 
 #-----------------------------------------------------------------------------#
 # Functions
 #-----------------------------------------------------------------------------#
 
-def get_news_of_company( ticker, current_time ):
+def get_news_of_company( ticker ):
     '''
     Description:   Gets all the news from Yahoo Finance for the company with the specified ticker symbol
     Inputs:        Ticker symbol of company
@@ -48,18 +48,7 @@ def get_news_of_company( ticker, current_time ):
     # Get all the news links on yahoo finance into a list
     links           = [ 'www.finance.yahoo.com/' + k.find_next('a').get('href') for k in soup.find_all('h3') ]
     # Get all the newspaper names that published the articles into a list
-    newspaper       = [ k.find_next('span').text for k in soup.find_all( class_ = 'C(#959595)') if "ago" in k.text ]
-    # Get time
-    timestamp       = [ k.find_next('span').find_next('span').text for k in soup.find_all( class_ = 'C(#959595)') if "ago" in k.text ]
-    for k in range(len(timestamp)):
-        if "minutes" in timestamp[k]:
-            timestamp[k] = round( current_time - float(timestamp[k].replace(" minutes ago", "")) / 60,2)
-        elif "hours" in timestamp[k]:
-            timestamp[k] = round( current_time - float(timestamp[k].replace(" hours ago", "")) )
-        elif "hour" in timestamp[k]:
-            timestamp[k] = round( current_time - float(timestamp[k].replace(" hour ago", "")) )
-        else:
-            timestamp[k] = np.nan
+    newspaper       = [ k.find_next('span').text for k in soup.find_all( class_ = 'C(#959595)') ]
     # Get the types of news into a list (Video or Article) based on the news tag on Yahoo Finance
     types           = []
     for k in range(len(newspaper)):
@@ -70,7 +59,7 @@ def get_news_of_company( ticker, current_time ):
     # Generalise the newspaper names by removing "Videos"
     newspaper       = [ k.replace(" Videos","") for k in newspaper ]
     # Create dictionary with the scraped data to write to DataFrame
-    data            = { "Ticker": ticker, "Date": today, "Headline": headers, "Link": links, "Description": descriptions, "Newspaper": newspaper, "Type": types, "Time": timestamp }
+    data            = { "Ticker": ticker, "Date": today, "Headline": headers, "Link": links, "Description": descriptions, "Newspaper": newspaper, "Type": types }
     return pd.DataFrame(data)
 
 #-----------------------------------------------------------------------------#
@@ -81,25 +70,20 @@ def get_news_of_company( ticker, current_time ):
 engine              = db.create_engine('mysql+pymysql://root:advnum19@localhost/dataserver')
 connectionObject    = engine.connect()
 
-## DB Connection (includes loading of relevant data)
-db = DBConn()
-# Get 'Ticker's from the 'Underlyings' table
-ticker_list         = db._getTickers()
+# Get 'Tickers' from the 'Underlyings' table
+selectTickersQuery  = "select Ticker from Underlyings"
+ticker_list         =  connectionObject.execute(selectTickersQuery)
 
 # Create 'news_df' DataFrame
 columns             = [ "Ticker", "Date", "Headline", "Link", "Description", "Newspaper", "Type" ]
 news_df             = pd.DataFrame( columns = columns)
 
-# Get current time in decimal format
-time                = datetime.datetime.now()
-time                = round( time.hour + time.minute / 60, 2)
-
 # Loop through ticker list to get news data from Yahoo Finance
 for ticker in ticker_list:
-    news_df         = news_df.append(get_news_of_company( ticker['Ticker'], time ), ignore_index = True, sort = False) # ticker['Ticker']
+    news_df         = news_df.append(get_news_of_company(ticker['Ticker']), ignore_index = True, sort = False)
 
 # Write 'news_df' DataFrame to the 'TickerNews' table in the dataserver database
-news_df.to_sql(name = "News", con = engine, if_exists='append', index = False)
+news_df.to_sql(name = "TickerNews", con = engine, if_exists='append', index = False)
 
 # Close the database connection
 connectionObject.close()
