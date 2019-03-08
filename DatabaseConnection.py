af4c90e55ebc35697084d57f0a9f5efa3b14f7ed
Lars Stauffenegger <lst@ads.cat-holding.ch>
2019-03-08 11:30:55 +0100

    create Method for DB communication

---
+++ DatabaseConnection.py
@@ -0,0 +1,44 @@
+## DB Connection
+
+## Import Packages
+import pymysql
+import os
+
+## Class as Instance of a Requests Session
+class DBConn():
+    
+    def __init__(self):
+        dbServerName    = "localhost"
+        self.dbUser     = "root"
+        dbPassword      = "advnum19"
+        self.dbName     = "dataserver"
+        charSet         = "utf8mb4"
+        cursorType      = pymysql.cursors.DictCursor
+        
+        # Cursor
+        self.connectionObject = pymysql.connect(host=dbServerName, user=self.dbUser, password=dbPassword, db=self.dbName, charset=charSet,cursorclass=cursorType)
+        self.cursorObject = self.connectionObject.cursor()
+        
+        # Load Data
+        self.tickerObject = self._getTickers()
+        self.quandlDataTypeObject = self._getQuandlDataType()
+        self.apiKeyObject = self._getAPIKey()
+        
+    def _getTickers(self):
+        self.cursorObject.execute("select Ticker from Underlyings")
+        return self.cursorObject.fetchall()
+    
+    def _getQuandlDataType(self):
+        self.cursorObject.execute("select DataType from RequestData where Source = '{}' ".format(os.environ.get('source', None)))
+        return self.cursorObject.fetchall()[0]["DataType"]
+    
+    def _getAPIKey(self):
+        self.cursorObject.execute("select APIKey from Authentications where User = '{}' ".format(self.dbUser))
+        return self.cursorObject.fetchall()[0]["APIKey"]
+    
+    def CloseConn(self):
+        # Close the database connection
+        self.connectionObject.close()
+
+db=DBConn()
+db.CloseConn()
--- EODQuandl.py
+++ EODQuandl.py
@@ -1,46 +1,24 @@
 ## Get Quandl Prices
 # Packages
-import getpass
-import pymysql
 import quandl
+import os
 
-# Manual Inputs
-username = "root"
-source = "Quandl"
-dbServerName    = "localhost"
-dbUser          = "root"
-dbPassword      = "advnum19"
-dbName          = "dataserver"
-charSet         = "utf8mb4"
-cursorType      = pymysql.cursors.DictCursor
+## Environmental Variable
+os.environ["source"] = "Quandl"
 
-# DB Connection
-connectionObject = pymysql.connect(host=dbServerName, user=dbUser, password=dbPassword, db=dbName, charset=charSet,cursorclass=cursorType)
-cursorObject = connectionObject.cursor()
+## DB Connection (includes loading of relevant data)
+db = DBConn()
 
-# SQL Queries
-selectTickersQuery = "select Ticker from Underlyings"
-selectRequestDataTypeQuery = "select DataType from RequestData where Source = '{}' ".format(source)
-selectAPIKeyQuery = "select APIKey from Authentications where User = '{}' ".format(username)
-
-# Get Tickers
-cursorObject.execute(selectTickersQuery)
-tickersObject = cursorObject.fetchall()
-
-# Get Request Data Type
-cursorObject.execute(selectRequestDataTypeQuery)
-requestDataTypeObject = cursorObject.fetchall()
-requestDataType = requestDataTypeObject[0]["DataType"]
-
-# Get Authentication Key
-cursorObject.execute(selectAPIKeyQuery)
-quandlApiKeyObject = cursorObject.fetchall()
-quandl.ApiConfig.api_key = quandlApiKeyObject[0]["APIKey"]
+# Quandl Authentication
+quandl.ApiConfig.api_key = db.apiKeyObject
 
 # Fetch Prices and Insert to DB
-for ticker in tickersObject:
+for ticker in db.tickerObject:
 	id = ticker["Ticker"]
-	quandlData = quandl.get(requestDataType + "/" + id, rows = 1)
+	quandlData = quandl.get(db.quandlDataTypeObject + "/" + id, rows = 1)
 	date = quandlData.index[0].date().strftime('%Y-%m-%d')
-	cursorObject.execute("INSERT IGNORE INTO Prices (Date, Ticker, Open, High, Low, Close, Volume, Dividend, Split, Adj_Open, Adj_High, Adj_Low, Adj_Close, Adj_Volume) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(date, id, float(quandlData.Open[0]), float(quandlData.High[0]), float(quandlData.Low[0]), float(quandlData.Close[0]), float(quandlData.Volume[0]), float(quandlData.Dividend[0]), float(quandlData.Split[0]), float(quandlData.Adj_Open[0]), float(quandlData.Adj_High[0]), float(quandlData.Adj_Low[0]), float(quandlData.Adj_Close[0]), float(quandlData.Adj_Volume[0])))
-	connectionObject.commit()
+	db.cursorObject.execute("INSERT IGNORE INTO Prices (Date, Ticker, Open, High, Low, Close, Volume, Dividend, Split, Adj_Open, Adj_High, Adj_Low, Adj_Close, Adj_Volume) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",(date, id, float(quandlData.Open[0]), float(quandlData.High[0]), float(quandlData.Low[0]), float(quandlData.Close[0]), float(quandlData.Volume[0]), float(quandlData.Dividend[0]), float(quandlData.Split[0]), float(quandlData.Adj_Open[0]), float(quandlData.Adj_High[0]), float(quandlData.Adj_Low[0]), float(quandlData.Adj_Close[0]), float(quandlData.Adj_Volume[0])))
+	db.connectionObject.commit()
+
+db.CloseConn()
+
